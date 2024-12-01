7b7e3a9a211a53373bf580d18217b54b5c36dbd5
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-01 20:34:32 +0100

    feat(cut-editor): use mid layout for algorithm parameters

--- src/main/java/dev/caceresenzo/barcutoptimizer/ui/component/AlgorithmSettingPanel.java
+++ src/main/java/dev/caceresenzo/barcutoptimizer/ui/component/AlgorithmSettingPanel.java
@@ -11,6 +11,7 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -19,96 +20,93 @@ import dev.caceresenzo.barcutoptimizer.optimize.AlgorithmManager.AlgorithmSettin
 
 @SuppressWarnings("serial")
 public class AlgorithmSettingPanel extends JPanel {
-	
+
 	/* Components */
 	private JTextField textField;
 	private JLabel actualValueLabel;
-	
+
 	/* Variables */
 	private final AlgorithmSettingEntry algorithmSettingEntry;
-	
+
 	/**
 	 * Create the panel.
 	 */
 	public AlgorithmSettingPanel(AlgorithmSettingEntry algorithmSettingEntry) {
-		setSize(400, 70);
-		setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		setSize(269, 62);
+		setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), AlgorithmManager.getTranslatedName(algorithmSettingEntry), TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		this.algorithmSettingEntry = algorithmSettingEntry;
-		
-		JLabel label = new JLabel(AlgorithmManager.getTranslatedName(algorithmSettingEntry));
-		label.setToolTipText(AlgorithmManager.getTranslatedDescription(algorithmSettingEntry));
-		
+
+		JLabel label = new JLabel(AlgorithmManager.getTranslatedDescription(algorithmSettingEntry));
+
 		textField = new JTextField();
-		textField.setColumns(10);
 		textField.setText(String.valueOf(algorithmSettingEntry.getValue()));
-		
+
 		actualValueLabel = new JLabel("actual value");
 		actualValueLabel.setHorizontalAlignment(SwingConstants.TRAILING);
-		actualValueLabel.setToolTipText((String) null);
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-								.addContainerGap()
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addGroup(groupLayout.createSequentialGroup()
-												.addGap(10)
-												.addComponent(textField, GroupLayout.DEFAULT_SIZE, 366, Short.MAX_VALUE))
-										.addGroup(groupLayout.createSequentialGroup()
-												.addComponent(label)
-												.addPreferredGap(ComponentPlacement.RELATED, 197, Short.MAX_VALUE)
-												.addComponent(actualValueLabel, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE)))
-								.addContainerGap()));
+							.addGap(10)
+							.addComponent(textField, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(label, GroupLayout.DEFAULT_SIZE, 174, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(actualValueLabel, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
 		groupLayout.setVerticalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-								.addContainerGap()
-								.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-										.addComponent(label)
-										.addComponent(actualValueLabel, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(textField, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
-								.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(label)
+						.addComponent(actualValueLabel, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
 		setLayout(groupLayout);
-		
+
 		updateUI();
-		
-		setMaximumSize(getSize());
-		setPreferredSize(getSize());
-		
+
 		validateInput(algorithmSettingEntry.getValue());
 		initializeTextFields();
 	}
-	
+
 	private void initializeTextFields() {
 		textField.getDocument().addDocumentListener(new DocumentListener() {
+
 			@Override
 			public void removeUpdate(DocumentEvent event) {
 				// System.out.println("remove");
 				sendUpdate();
 			}
-			
+
 			@Override
 			public void insertUpdate(DocumentEvent event) {
 				// System.out.println("insert");
 				sendUpdate();
 			}
-			
+
 			@Override
 			public void changedUpdate(DocumentEvent event) {
 				// System.out.println("change");
 				sendUpdate();
 			}
-			
+
 			private void sendUpdate() {
 				SwingUtilities.invokeLater(() -> validateInput(textField.getText()));
 			}
+
 		});
 	}
-	
+
 	private void validateInput(Object object) {
 		boolean success = false;
-		
+
 		if (object instanceof String) {
 			try {
 				success = algorithmSettingEntry.setValueWithAutoParsing((String) object);
@@ -118,18 +116,18 @@ public class AlgorithmSettingPanel extends JPanel {
 		} else {
 			success = algorithmSettingEntry.setValue(object);
 		}
-		
+
 		if (success) {
 			textField.setToolTipText("");
 		}
-		
+
 		textField.setForeground(success ? Color.BLACK : Color.RED);
-		
+
 		actualValueLabel.setText(String.valueOf(algorithmSettingEntry.getValue()));
 	}
-	
+
 	public JLabel getActualValueLabel() {
 		return actualValueLabel;
 	}
-	
+
 }
