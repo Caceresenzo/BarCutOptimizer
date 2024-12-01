25b5a5011da783802e86d268ca1e14f058d37a2c
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-01 01:16:47 +0100

    feat(editor): default reference name

--- src/main/java/dev/caceresenzo/barcutoptimizer/ui/other/NewBarReferenceDialogs.java
+++ src/main/java/dev/caceresenzo/barcutoptimizer/ui/other/NewBarReferenceDialogs.java
@@ -11,32 +11,36 @@ import dev.caceresenzo.barcutoptimizer.model.BarReference;
 import dev.caceresenzo.barcutoptimizer.ui.BarCutOptimizerWindow;
 
 public class NewBarReferenceDialogs {
-	
+
 	/* Singleton */
 	private static NewBarReferenceDialogs INSTANCE;
-	
+
 	/* Private Constructor */
 	private NewBarReferenceDialogs() {
 		;
 	}
-	
+
 	public BarReference openBarReferenceCreationDialog() {
-		String name = JOptionPane.showInputDialog(BarCutOptimizerWindow.get().getWindow(), I18n.string("dialog.new-bar-reference.message"));
-		
+		String name = JOptionPane.showInputDialog(
+			BarCutOptimizerWindow.get().getWindow(),
+			I18n.string("dialog.new-bar-reference.message"),
+			"ref"
+		);
+
 		if (StringUtils.isBlank(name)) {
 			return null;
 		}
-		
+
 		return new BarReference(name, new ArrayList<>());
 	}
-	
+
 	/** @return NewBarReferenceDialogs's singleton instance. */
 	public static final NewBarReferenceDialogs get() {
 		if (INSTANCE == null) {
 			INSTANCE = new NewBarReferenceDialogs();
 		}
-		
+
 		return INSTANCE;
 	}
-	
+
 }
