cfded546ba8827d21791ebc6f16dd4a209aad0e4
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-01 23:23:37 +0100

    chore: remove unused code

--- src/main/java/dev/caceresenzo/barcutoptimizer/BarCutOptimizer.java
+++ src/main/java/dev/caceresenzo/barcutoptimizer/BarCutOptimizer.java
@@ -1,7 +1,6 @@
 package dev.caceresenzo.barcutoptimizer;
 
 import java.io.File;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -23,26 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class BarCutOptimizer {
-
-	public static final File JAR_FILE;
-	public static final File APPLICATION_FOLDER;
-	public static final File CACHE_FOLDER;
-
-	static {
-		File jarFile = null;
-		try {
-			jarFile = new File(BarCutOptimizer.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-		} catch (URISyntaxException exception) {
-			System.err.println("Failed to locate the jar archive file.");
-			System.exit(1);
-		}
-
-		JAR_FILE = jarFile;
-		APPLICATION_FOLDER = JAR_FILE.getParentFile();
-		CACHE_FOLDER = new File(APPLICATION_FOLDER, "cache");
-
-		CACHE_FOLDER.deleteOnExit();
-	}
 
 	public static void main(String[] args) {
 		I18n.load(Locale.FRENCH);
