940b09d4a1bc02bf27efd8c09fe186f1133e2211
Caceresenzo <caceresenzo1502@gmail.com>
2024-12-11 00:27:44 +0100

    fix(setup/shortcuts): missing quotes

--- setup/register-shortcuts.cmd
+++ setup/register-shortcuts.cmd
@@ -2,6 +2,6 @@
 
 CD "%~DP0\..\"
 
-".\setup\shortcut.exe" /a:c /i:"%cd%\icon.ico" /f:"C:\Users\%username%\Desktop\Optimiseur de Coupe.lnk" /t:"%cd%\jre21\bin\javaw.exe" /p:"-jar %cd%\bar-cut-optimizer.jar"
+".\setup\shortcut.exe" /a:c /i:"%cd%\icon.ico" /f:"C:\Users\%username%\Desktop\Optimiseur de Coupe.lnk" /t:"%cd%\jre21\bin\javaw.exe" /p:"-jar \"%cd%\bar-cut-optimizer.jar\""
 
-".\setup\shortcut.exe" /a:c /i:"%cd%\icon.ico" /f:"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Optimiseur de Coupe.lnk" /t:"%cd%\jre21\bin\javaw.exe" /p:"-jar %cd%\bar-cut-optimizer.jar"
+".\setup\shortcut.exe" /a:c /i:"%cd%\icon.ico" /f:"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Optimiseur de Coupe.lnk" /t:"%cd%\jre21\bin\javaw.exe" /p:"-jar \"%cd%\bar-cut-optimizer.jar\""
